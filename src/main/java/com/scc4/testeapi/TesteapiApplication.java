b6aaf4812715dc85de674d25f202df7af69ae30a
SSteringS <satyan.stering@gmail.com>
2019-07-08 14:24:13 -0300

    link do repositorio

---
+++ src/main/java/com/scc4/testeapi/TesteapiApplication.java
@@ -0,0 +1,13 @@
+package com.scc4.testeapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesteapiApplication.class, args);
+	}
+
+}
